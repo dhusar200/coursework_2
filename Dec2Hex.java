83d5cc58909f96d346fbb0ef2e3e3ce592b539d1
dhusar200 <dhusar200@caledonian.ac.uk>
2019-12-10 18:31:28 +0000

    code

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,39 +1,38 @@
 import java.util.Scanner;
 
-class Dec2Hex
-{
+class Dec2Hex {
 
     public static int Arg1;
 
-    public static void main(String args[])
-    {
-        if (args.length < 0) {
-            System.err.println("Argument must have an value");
-        }
-        else if (args.length > 0) {
-            try {
-                Arg1 = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                System.err.println("Argument" + args[0] + " must be an integer.");
-                System.exit(1);
+    public static void main(String args[]) {
+        for(int i = 0; i < args.length; i++) {
+            if (!args[i].equals("")) {
+                try {
+                    Arg1 = Integer.parseInt(args[i]);
+
+                    if (Arg1 > 0) {
+                        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+                        int rem, num;
+                        num = Arg1;
+                        String hexadecimal = "";
+                        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+                        while (num != 0) {
+                            rem = num % 16;
+                            hexadecimal = ch[rem] + hexadecimal;
+                            num = num / 16;
+                        }
+                        System.out.println("Hexadecimal representation is : " + hexadecimal);
+                    } else {
+                        System.err.println("Argument " + args[i] + " cannot be negative.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.err.println("Argument" + args[i] + " must be an integer.");
+                }
+            } else {
+                System.err.println("Argument needs to have an value.");
             }
-
-
-            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-            int rem, num;
-            num = Arg1;
-            String hexadecimal = "";
-            System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-            while (num != 0) {
-                rem = num % 16;
-                hexadecimal = ch[rem] + hexadecimal;
-                num = num / 16;
-            }
-
-            System.out.println("Hexadecimal representation is : " + hexadecimal);
         }
-	System.out.println("Thank you for using our azz");
-
+        System.out.println("Thank you for using our application!");
     }
 }
